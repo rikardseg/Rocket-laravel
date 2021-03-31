6c230de88c297f62c210fda01abefefed5ff99e8
rikardseg <70521759+rikardseg@users.noreply.github.com>
2021-03-31 19:43:51 +0200

    databases

--- database/migrations/2021_03_31_131655_create_order_history_table.php
+++ database/migrations/2021_03_31_131655_create_order_history_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFailedJobsTable extends Migration
+class CreateOrderHistoryTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateFailedJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('order', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->integer('latest_order');
+            $table->timestamps();
         });
     }
 
@@ -31,5 +27,6 @@ class CreateFailedJobsTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('order_history');
     }
 }
